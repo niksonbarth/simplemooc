e25968580bf45e7acd5455ff18c09b37af604d3c
niksonbarth <32281780+niksonbarth@users.noreply.github.com>
2017-10-11 19:45:14 -0300

    static

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -160,7 +160,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 try:
     from simplemooc.local_settings import *
