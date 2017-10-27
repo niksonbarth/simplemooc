55a975b4dd492292459ac4babdb3e5116e689640
nikson <nikson.barth@benner.com.br>
2017-10-27 13:04:31 -0200

    6:11

--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView, View, ListView
 
-class ForumView(TemplateView):
+from .models import Thread
 
+#class ForumView(TemplateView):
+#    template_name = 'forum/index.html'
+#index = ForumView.as_view()
+
+#index = TemplateView.as_view(template_name=template_name)
+
+class ForumView(ListView):
+
+    model = Thread
+    paginate_by = 10
     template_name = 'forum/index.html'
 
 index = ForumView.as_view()
-
-#index = TemplateView.as_view(template_name=template_name)
