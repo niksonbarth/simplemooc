ef7ab1b21308dd0a83cf76969215993accd81d4a
nikson <nikson.barth@benner.com.br>
2017-10-31 13:24:01 -0200

    Aula 83

--- simplemooc/forum/urls.py
+++ simplemooc/forum/urls.py
@@ -4,4 +4,5 @@ from simplemooc.forum import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^tag/(?P<tag>[\w_-]+)/$', views.index, name='index_tagged'),
+    url(r'^(?P<slug>[\w_-]+)/$', views.Thread, name='thread'),
 ]
--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, View, ListView
+from django.views.generic import TemplateView, View, ListView,DetailView
 
 from .models import Thread
 
@@ -21,17 +21,21 @@ class ForumView(ListView):
             queryset = queryset.order_by('-views')
         elif order == 'answers':
             queryset = queryset.order_by('-answers')
-        
+
         tag = self.kwargs.get('tag', '')
         if tag:
-            queryset = queryset.filter(tags__slug__icontains=tag)            
+            queryset = queryset.filter(tags__slug__icontains=tag)
 
         return queryset
-           
-       
+
     def get_context_data(self, **kwargs):
         context = super(ForumView, self).get_context_data(**kwargs)
         context['tags'] = Thread.tags.all()
         return context
+
+class ThreadView(DetailView):
+
+    model = Thread
+    template_name = 'forum/thread.html'
 
 index = ForumView.as_view()
